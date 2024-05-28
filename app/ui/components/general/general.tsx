cedd2c72ca45c98b6d5ecc529a9e2632ae613913
mohamed khounti <larbikhounti@gmail.com>
2024-05-28 19:36:39 +0100

    adding beta

--- app/ui/components/general/general.tsx
+++ app/ui/components/general/general.tsx
@@ -2,16 +2,20 @@
 import styles from "./general.module.scss"
 
 import { robotoBold, robotoRegular, robotoThin } from '@/app/ui/fonts';
+interface subbETA  {sub : string, beta : boolean}
 
-export default function General({category,subcategries} : {
-  category : String,
-  subcategries : string[],
+export default function General({category, subcategries  } : {
+  category : string,
+  subcategries : subbETA[],
 }) {
+  function itBeta(itIs : boolean) {
+    return itIs ? <div className={`${styles.beta} ${robotoThin.className}`}>beta</div> : ""
+  }
   
   return (
     <ul className={styles.general}>
       <h5 className={`${styles.category} ${robotoBold.className}`}>{category}</h5>
-      {subcategries.map(sub=> <li  key={sub} className={`${styles.subcategory} ${robotoThin.className}`}>{sub}</li>)}
+      {subcategries.map(sub=> <li  key={sub.sub} className={`${styles.subcategory} ${robotoThin.className}`}>{sub.sub} {itBeta(sub.beta)}</li>)}
     </ul>
   );
 }
--- app/ui/section6/section6.tsx
+++ app/ui/section6/section6.tsx
@@ -8,29 +8,37 @@ export default function SectionSix() {
   const links =[
     {
       category : "Product",
-      subCategories : ["Pricing", "Overview", "Browse", "Accessibility"]
+      subCategories : [{sub : "Pricing", beta: true}, {sub : "Overview", beta: false}, {sub : "Browse", beta: false}, {sub : "Accessibility", beta: false}]
+      
     },
     {
       category : "Solutions",
-      subCategories : ["Brainstorming", "Ideation", "Wireframing", "Research"]
+      subCategories : [{sub : "Brainstorming ", beta: false}, {sub : "Ideation", beta: false}, { sub : "Wireframing", beta: false} , {sub : "Research", beta: false}],
+       
     },
     {
       category : "Resources",
-      subCategories : ["Help Center", "Blog", "Tutorials", "FAQs"]
+      subCategories : [{sub : "Help Center", beta: false} , {sub : "Blog", beta: false}, {sub  : "Tutorials", beta: true},  {sub : "FAQs", beta: false}],
+       
     },
     {
       category : "Support",
-      subCategories : ["Contact Us", "Developers", "Documentation", "Integrations"]
+      subCategories : [{sub : "Contact Us", beta: true},  {sub : "Developers", beta: false},  {sub : "Documentation ", beta: false}, {sub : "Integrations", beta: false }],
+      
     },
     {
       category : "Company",
-      subCategories : ["About", "Press", "Events", "Request Demo"]
+      subCategories : [{sub : "About", beta: false}, {sub  : "Press", beta: false}, {sub  : "Events", beta: false}, {sub : "Request Demo", beta: false }]
     }
   ]
   return (
     <div className={styles.main}>
       <div><Image alt="Logo colored" src="./colored-logo-end.svg" width={172} height={35} /></div>
-      {links.map(cat=> <General key={cat.category} category={cat.category} subcategries={cat.subCategories} />)}
+      {links.map(cat=>
+        <General key={cat.category}
+        category={cat.category}
+        subcategries={cat.subCategories} />)
+      }
     </div>
   );
 }
